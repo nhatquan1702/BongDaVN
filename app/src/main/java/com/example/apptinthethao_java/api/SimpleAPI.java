1203d68798bb0b56c67a1c8fdbbac6c80a7d00fd
tncn1122 <tncnhan@gmail.com>
2021-06-06 15:25:17 +0700

    fix merge

--- app/src/main/java/com/example/apptinthethao_java/api/SimpleAPI.java
+++ app/src/main/java/com/example/apptinthethao_java/api/SimpleAPI.java
@@ -4,8 +4,10 @@ import com.example.apptinthethao_java.model.CauLacBo;
 import com.example.apptinthethao_java.model.CauThu_DoiHinh;
 import com.example.apptinthethao_java.model.DetailPost;
 import com.example.apptinthethao_java.model.DienBienTranDau;
+import com.example.apptinthethao_java.model.DoiHinh;
 import com.example.apptinthethao_java.model.KetQua_TranDau;
 import com.example.apptinthethao_java.model.Post;
+import com.example.apptinthethao_java.model.SuKienTrongTran;
 import com.example.apptinthethao_java.model.TranDau;
 
 import java.util.ArrayList;
@@ -35,6 +37,9 @@ public interface SimpleAPI {
 
     @GET("quan/tatcacauthu_clb/{clb_id}")
     Call<ArrayList<CauThu_DoiHinh>> getListCauThu(@Path("clb_id") String id);
+
+    @GET("quan/chitietclb/{clb_id}")
+    Call<ArrayList<CauLacBo>> getChiTietCLB(@Path("clb_id") String id);
 
 //    @GET("khoa/get_ketqua_nam/{year}")
 //    Call<ArrayList<KetQua_TranDau>> getListKetQuaTranDau(@Path("year") String year);
