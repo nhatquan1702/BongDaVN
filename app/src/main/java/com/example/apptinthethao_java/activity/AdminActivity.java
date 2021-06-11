9f286cd14bba6f9e6bc097cd9bad50910de7d4ac
Minato1803 <nguyenduckhai.otaku@gmail.com>
2021-06-11 10:17:05 +0700

    update from Minato
    -admin activity thêm chuyển hướng

--- app/src/main/java/com/example/apptinthethao_java/activity/AdminActivity.java
+++ app/src/main/java/com/example/apptinthethao_java/activity/AdminActivity.java
@@ -4,12 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.apptinthethao_java.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AdminActivity extends AppCompatActivity {
+public class AdminActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        Button btnTaiKhoan = findViewById(R.id.btnTaiKhoan);
+        Button btnBaiViet = findViewById(R.id.btnBaiViet);
+        Button btnCauThu = findViewById(R.id.btnCauThu);
+        Button btnTranDau = findViewById(R.id.btnTranDau);
+        Button btnDoiBong = findViewById(R.id.btnDoiBong);
+
+        btnTaiKhoan.setOnClickListener(this);
+        btnBaiViet.setOnClickListener(this);
+        btnCauThu.setOnClickListener(this);
+        btnTranDau.setOnClickListener(this);
+        btnDoiBong.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnTaiKhoan:{
+                // chuyen acti
+            }
+            case R.id.btnBaiViet:{
+                // chuyen acti
+            }
+            case R.id.btnCauThu:{
+                // chuyen acti
+            }
+            case R.id.btnTranDau:{
+                // chuyen acti
+            }
+            case R.id.btnDoiBong:{
+                // chuyen acti
+            }
+        }
     }
 }
